c936eb3272bcd30f419c14a5ef9a4ead75d853b2
banst <bastien@loftorbital.com>
2023-03-03 19:07:19 +0100

    fix: image in script

--- sync.sh
+++ sync.sh
@@ -5,11 +5,20 @@ set -e
 git tag > local.list
 tags=$(glab release list -R gitlab-org/gitlab-runner | tail -n +3 | awk '{ print $1 }' | grep -vf local.list)
 
+archs="x86_64 arm64 arm s390x ppc64le"
+
 for tag in $tags
 do
-  manifest-tool push from args \
+  for arch in $archs
+  do
+    skopeo copy --dest-creds="$:$GITHUB_TOKEN" \
+      docker://registry.gitlab.com/gitlab-org/gitlab-runner/gitlab-runner-helper:alpine-latest-$arch-$tag \
+      docker://ghcr.io/loft-orbital/gitlab-runner-helper:alpine-latest-$arch-$tag
+  done
+  
+  manifest-tool push from-args \
     --platforms linux/x86_64,linux/arm64,linux/arm,linux/s390x,linux/ppc64le \
-    --template registry.gitlab.com/gitlab-org/gitlab-runner/gitlab-runner-helper:alpine-latest-ARCH-$(version):$tag \
+    --template ghcr.io/loft-orbial/gitlab-runner-helper:alpine-latest-ARCH-$tag \
     --target ghcr.io/loft-orbital/gitlab-runner-helper:$tag
   gh release create $tag --generate-notes
 done
